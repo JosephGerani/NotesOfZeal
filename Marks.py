c39789720e0f69a4044be8a764893860ae98ab99
JosephGerani <89358015+JosephGerani@users.noreply.github.com>
2021-11-19 22:37:15 -0800

    Markdown to HTML functionality added
    This was way easier than I expected. Python already supports this thanks to the Markdown library so barely any code was needed.

---
+++ Marks.py
@@ -0,0 +1,15 @@
+#Amazingly simple how easy it is to convert markdown to HTML
+#thanks to this goofy library
+import markdown
+
+def MarkdownToHTML():
+    Bruh=input("Enter the filename ")
+    with open(Bruh, 'r') as f:
+        text = f.read()
+        html = markdown.markdown(text)
+    Bruh2=input("Enter the filename you want to save it as (don't add html) ")
+    Bruh2=Bruh2+".html"
+    with open(Bruh2, 'w') as f:
+        f.write(html)
+
+MarkdownToHTML()
